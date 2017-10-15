ed358776a88ece6471324525b462c38be235390a
Anthony Mai <s3550974@student.rmit.edu.au>
2017-10-15 18:20:02 +1100

    Just making a backup of progress on tunnel maze as well as the start of the debugging of backtracker solver. Made wall solver code neater.

--- mazeSolver/BiDirectionalRecursiveBacktrackerSolver.java
+++ mazeSolver/BiDirectionalRecursiveBacktrackerSolver.java
@@ -1,30 +1,68 @@
 package mazeSolver;
 
-import maze.Maze;
+import java.util.*;
+import maze.*;
 
-/**
+/*
  * Implements the BiDirectional recursive backtracking maze solving algorithm.
  */
 public class BiDirectionalRecursiveBacktrackerSolver implements MazeSolver {
+	/*
+	 * Declare global variables
+	 * (I know it's bad practice but it's so easy to work with ;-;)
+	 */
+	boolean flagEnd = false;
+	int visitedCounter = 0;
+	int mazeType = -1;
+	Cell[][] map;
+	boolean[][] visitedCells;
+	Cell curPos;
+	int curDir;
 
 	@Override
-	public void solveMaze(Maze maze) {
-		// TODO Auto-generated method stub
+	public void solveMaze(Maze maze){
+		Stack<Cell> stackCells = new Stack<Cell>();
+		//assign values to the global variables
+		map = maze.map;
+		curPos = maze.entrance;
+		curDir = maze.EAST;
+		mazeType = maze.type;
+		if (mazeType == 2)
+			visitedCells = new boolean[maze.sizeR][maze.sizeC + (maze.sizeR + 1) / 2];
+		else
+			visitedCells = new boolean[maze.sizeR][maze.sizeC];
 
+		//debug
+		for(int i = 0; i < 6; i++){
+			System.out.println(curPos.neigh[i]);
+		}
+
+		//working space
+		stackCells.push(curPos);
+		while(flagEnd == false){
+			stackCells.push(curPos);
+		}
 	} // end of solveMaze()
 
+	private Cell getRandomNeighbour(Cell pos){
+		Cell[] possibleNeigh = new Cell[6];
+		for(int i = 0; i < 6; i++){
+			if(curPos.neigh[i] != null){
+
+			}
+		}
+		return null;
+	}
+
 
 	@Override
-	public boolean isSolved() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isSolved(){
+		return flagEnd;
 	} // end if isSolved()
 
 
 	@Override
-	public int cellsExplored() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int cellsExplored(){
+		return visitedCounter;
 	} // end of cellsExplored()
-
 } // end of class BiDirectionalRecursiveBackTrackerSolver
--- mazeSolver/WallFollowerSolver.java
+++ mazeSolver/WallFollowerSolver.java
@@ -6,115 +6,114 @@ import maze.*;
 /*
  * Implements WallFollowerSolver
  */
-
 public class WallFollowerSolver implements MazeSolver{
+	/*
+	 * Declare global variables
+	 * (I know it's bad practice but it's so easy to work with, pls no bully ;-;)
+	 */
+	// boolean flag to stop while loop once it finds exit
 	boolean flagEnd = false;
+	// counter of the amount of cells traversed
 	int visitedCounter = 0;
+	// declares the maze type,
 	int mazeType = -1;
+	Cell[][] map;
+	boolean[][] visitedCells;
+	Cell curPos;
+	int curDir;
 
+	/*
+	 * Left hand wall follower search
+	 *
+	 * ******************************************************************************************
+	 *
+	 * ALGORITHM DFS ( G )
+	 * Perform a left hand wall follower search on the maze
+	 * Input: Maze of cells declared by the variable "maze"
+	 * OUTPUT : Maze "maze" marked with grey circles to show visited cells.
+	 *
+	 * ******************************************************************************************
+	 *
+	 * @param maze Input Maze.
+	 */
 	@Override
 	public void solveMaze(Maze maze){
-		Cell map[][] = maze.map;
-		boolean visitedTunnels[][];
-		visitedTunnels = new boolean[maze.sizeR][maze.sizeC];
-		Cell curPos = maze.entrance;
-		int curDir = maze.EAST;
+		//assign values to the global variables
+		map = maze.map;
+		visitedCells = new boolean[maze.sizeR][maze.sizeC];
+		curPos = maze.entrance;
+		curDir = maze.EAST;
 		mazeType = maze.type;
+		//initial move at start of maze
 		maze.drawFtPrt(curPos);
 		visitedCounter++;
 
-		//debug
 		/*
-		System.out.println("r = " + curPos.r);
-		System.out.println("c = " + curPos.c);
-		System.out.println("East = " + curPos.wall[maze.EAST].present);
-		System.out.println("West = " + curPos.wall[maze.WEST].present);
-		System.out.println("NW = " + curPos.wall[maze.NORTHWEST].present);
-		System.out.println("NE = " + curPos.wall[maze.NORTHEAST].present);
-		System.out.println("SW = " + curPos.wall[maze.SOUTHWEST].present);
-		System.out.println("SE = " + curPos.wall[maze.SOUTHEAST].present);
-		for(int i=0;i<6;i++){
-			curDir = antiClockwise(curDir);
-			System.out.println("ACW[" + i + "]" + " = " + curDir);
-		}
-		for(int i=0;i<6;i++){
-			curDir = clockwise(curDir);
-			System.out.println("CW[" + i + "]" + " = " + curDir);
-		}
-		for(int i=0;i<3;i++){
-			curPos = map[curPos.r + maze.deltaR[maze.SOUTHWEST]][curPos.c + maze.deltaC[maze.SOUTHWEST]];
-			maze.drawFtPrt(curPos);
-		}
-		for(int i=0;i<3;i++){
-			curPos = map[curPos.r + maze.deltaR[maze.WEST]][curPos.c + maze.deltaC[maze.WEST]];
-			maze.drawFtPrt(curPos);
-		}
+		//debug
+		try
+			Thread.sleep(1000);
+		catch(InterruptedException ex)
+			Thread.currentThread().interrupt();
 		*/
 
-		//workspace
+		//solver loop
 		while(flagEnd == false){
-			/*
-			//Debug
-			try{
-    		Thread.sleep(1000);
-			}
-			catch(InterruptedException ex){
-    		Thread.currentThread().interrupt();
-			}
-			*/
 			if(curPos == maze.exit)
 				flagEnd = true;
-			else if(curPos.tunnelTo != null && visitedTunnels[curPos.r][curPos.c] == false){
-				visitedTunnels[curPos.r][curPos.c] = true;
+			//if tunnel is detected
+			else if(curPos.tunnelTo != null){
+				System.out.println("Tunnel = " + curPos.tunnelTo.r + " " + curPos.tunnelTo.c);
 				curPos = curPos.tunnelTo;
 				maze.drawFtPrt(curPos);
 				visitedCounter++;
-				while(curPos.tunnelTo != null){
-					if(curPos.wall[antiClockwise(curDir)].present == false){
-						curDir = antiClockwise(curDir);
-						curPos = map[curPos.r + maze.deltaR[curDir]][curPos.c + maze.deltaC[curDir]];
-						maze.drawFtPrt(curPos);
-						visitedCounter++;
-					}
-					else if(curPos.wall[curDir].present == false){
-						curPos = map[curPos.r + maze.deltaR[curDir]][curPos.c + maze.deltaC[curDir]];
-						maze.drawFtPrt(curPos);
-						visitedCounter++;
+				int flagTunnel = 0;
+				while(flagTunnel >= 0){
+					System.out.println("inside flagTunnel loop");
+					//if there is no front wall
+					if(curPos.wall[curDir].present == false
+					&& (visitedCells[curPos.r + maze.deltaR[curDir]][curPos.c + maze.deltaC[curDir]] == false
+					|| flagTunnel > 4)){
+						moveForward(maze);
+						flagTunnel = -1;
 					}
 					else{
 						curDir = clockwise(curDir);
+						++flagTunnel;
+						System.out.println(flagTunnel);
 					}
 				}
 			}
 			//if there is no left wall
 			else if(curPos.wall[antiClockwise(curDir)].present == false){
 				curDir = antiClockwise(curDir);
-				curPos = map[curPos.r + maze.deltaR[curDir]][curPos.c + maze.deltaC[curDir]];
-				maze.drawFtPrt(curPos);
-				visitedCounter++;
-				//below is needed to avoid logic error of current method ;-;
-				if(mazeType == 2){
-					curDir = antiClockwise(curDir);
-				}
+				moveForward(maze);
 			}
 			//if there is no front wall
 			else if(curPos.wall[curDir].present == false){
-				curPos = map[curPos.r + maze.deltaR[curDir]][curPos.c + maze.deltaC[curDir]];
-				maze.drawFtPrt(curPos);
-				visitedCounter++;
-				//below is needed to avoid logic error of current method ;-;
-				if(mazeType == 2){
-					curDir = antiClockwise(curDir);
-				}
+				moveForward(maze);
 			}
+			//else rotate clockwise
 			else{
 				curDir = clockwise(curDir);
 			}
 		}// end of while
 	} // end of solveMaze()
 
+	//moves curPos forward
+	private void moveForward(Maze maze){
+		curPos = map[curPos.r + maze.deltaR[curDir]][curPos.c + maze.deltaC[curDir]];
+		maze.drawFtPrt(curPos);
+		visitedCounter++;
+		//visitedCells only used for tunnels
+		if(mazeType == 1)
+			visitedCells[curPos.r][curPos.c] = true;
+		//below is needed to avoid logic error of current method ;-;
+		if(mazeType == 2)
+			curDir = antiClockwise(curDir);
+	}
+
 	//finds direction that is anticlockwise of the current direction
-	public int antiClockwise(int dir){
+	private int antiClockwise(int dir){
 		if(mazeType == 2){
 			if(dir == 5)
 				return 0;
